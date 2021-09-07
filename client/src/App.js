39fa8bd59ab862c9daea7fdd7762f9fb6334b1ec
Chris Malilay <malilaychris@gmail.com>
2021-09-07 04:13:09 -0500

    Initialize React front end structure

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>College Carball Association</h1>
+  </div>
+);
+
+export default App;
---
+++ config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, './client/src'), 'node_modules'],
+    extensions: ['.js', '.jsx', '.json']
+  },
+  plugins: [
+    new htmlWebpackPlugin ({
+      template: './client/public/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
