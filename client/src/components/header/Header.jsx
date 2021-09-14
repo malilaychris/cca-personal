c4e07379210b81b543f556b53a71046c48a9390b
Chris Malilay <malilaychris@gmail.com>
2021-09-14 18:06:45 -0500

    Add early version of navigation to header component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import './Header.css';
+
+import Logo from '../../assets/images/ccalogonav.svg';
+
 const Header = () => (
-  <header></header>
+  <header className="header">
+    <div className="container">
+      <div className="logo-container">
+        <a href="/"><img className="logo" src={Logo} /></a>
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li><a className="nav-item" href="/">Home</a></li>
+          <li><a className="nav-item" href="/">About</a></li>
+          <li><a className="nav-item" href="/">Chapters</a></li>
+          <li><a className="nav-item" href="/">News</a></li>
+          <li><a className="nav-item" href="/">Rules</a></li>
+        </ul>
+        <div className="nav-fill" />
+        <ul className="nav-list nav-list-secondary">
+          <li><a className="nav-item nav-item-secondary" href="/">Store</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 );
 
 export default Header;
