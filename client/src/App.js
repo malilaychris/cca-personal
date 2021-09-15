c656e864cf7847dc81980ece019f1fdf46b19769
Chris Malilay <malilaychris@gmail.com>
2021-09-15 02:31:07 -0500

    Add chapters page view and route

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './assets/styles/App.css';
 
-import { Home } from './views';
+import { Home, Chapters } from './views';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/chapters" component={Chapters} />
     </Switch>
   </Router>
 );
--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,3 +1,4 @@
 import Home from './Home';
+import Chapters from './Chapters';
 
-export { Home };
+export { Home, Chapters };
