d36f9f3a8eb6aa8dc00c72372d3385a6b2f163e8
Chris Malilay <malilaychris@gmail.com>
2021-09-16 05:04:38 -0500

    Fix router from being unable to load pages with manual URL input in production

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
@@ -6,6 +7,12 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server starting on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`);
 });
