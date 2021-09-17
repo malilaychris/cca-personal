a4d82a757108d978b889ee71a66eeec3ee4f142d
Chris Malilay <malilaychris@gmail.com>
2021-09-16 22:20:51 -0500

    Initialize early RESTful API for displaying chapter list, adding and removing chapters

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const database = require('./database');
 
 const PORT = 3000;
 
@@ -8,6 +9,75 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
+
+/*
+  Chapters RESTful API
+*/
+
+// get all chapters
+app.get('/api/chapters', async (req, res) => {
+  try {
+    const chapters = await database.query(
+      'SELECT * FROM chapters'
+    );
+
+    res.json(chapters.rows);
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+
+//get chapter by ID
+app.get('/api/chapters/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getChapter = await database.query(
+      'SELECT * FROM chapters WHERE id = $1',
+      [id]
+    );
+
+    res.json(getChapter.rows[0]);
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+
+//add chapter
+app.post('/api/chapters', async (req, res) => {
+  try {
+    const { school, id } = req.body;
+    const addChapter = await database.query(
+      'INSERT INTO chapters(school, id) VALUES($1, $2) RETURNING *',
+      [school, id]
+    );
+
+    res.json({
+      message: `Chapter [${school}] has been added at ID: [${id}]`,
+      chapter: addChapter.rows[0]
+    });
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+
+// delete chapter
+app.delete('/api/chapters/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const chapter = await database.query(
+      'SELECT school FROM chapters WHERE id = $1',
+      [id]
+    );
+    const deleteChapter = await database.query(
+      'DELETE FROM chapters WHERE id = $1',
+      [id]
+    );
+
+    res.json(`Chapter [${chapter.rows[0].school}] at ID: [${id}] has been deleted`);
+  } catch(err) {
+    console.error(err.message);
+  }
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
