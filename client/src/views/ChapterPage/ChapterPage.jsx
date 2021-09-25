ca87633f5e69c6cf596e84ee5da00d5d12a07f7b
Chris Malilay <malilaychris@gmail.com>
2021-09-25 16:27:55 -0500

    Add chapter page sample, modified navbar animations

---
+++ client/src/views/ChapterPage/ChapterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ChapterPage = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="page-title">University of Texas at Austin</h1>
+      </div>
+    </>
+  );
+};
+
+export default ChapterPage;
--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,7 +1,8 @@
 import Home from './Home';
 import About from './About';
 import Chapters from './Chapters';
+import ChapterPage from './ChapterPage';
 import News from './News';
 import Rules from './Rules';
 
-export { Home, About, Chapters, News, Rules };
+export { Home, About, Chapters, ChapterPage, News, Rules };
