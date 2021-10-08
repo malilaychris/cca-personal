599a37b1da090cd5414727836b5954e863f7ccc2
Chris Malilay <malilaychris@gmail.com>
2021-10-08 04:11:30 -0500

    Move chapter page render logic inside render body

--- client/src/views/ChapterPage/ChapterPage.jsx
+++ client/src/views/ChapterPage/ChapterPage.jsx
@@ -27,54 +27,47 @@ const ChapterPage = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  let chapterTitle = String(chapterInfo.school);
-  let chapterTitleLong;
-  let chapterLocation;
-  let chapterRegion;
-
-  if(chapterTitle.length > 46) {
-    chapterTitleLong = 'chapter-title-long';
-  }
-
-  if(chapterInfo.country) {
-    chapterLocation = chapterInfo.country;
-    if(chapterInfo.state) {
-      chapterLocation = `${chapterInfo.state} - ` + chapterLocation;
-      if(chapterInfo.city) {
-        chapterLocation = `${chapterInfo.city}, ` + chapterLocation;
-      }
-    }
-  }
-
-  if(chapterInfo.region) {
-    chapterRegion = `${chapterInfo.region} Conference`;
-  }
-
   return (
     <>
       <div className="container">
 
         <div className="chapter-header">
           <div className="chapter-colors">
-            <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="68" height="68" viewBox="0 0 68 68">
-              <defs>
-                <clipPath id="clipPath">
-                  <rect id="rectangleMask" data-name="rectangleMask" width="68" height="68" rx="7"/>
-                </clipPath>
-              </defs>
-              <g id="chapterColors" data-name="chapterColors">
-                <g id="mask1" data-name="mask1" clipPath="url(#clipPath)">
-                  <path id="color1" data-name="color1" d="M0,0H68.3S55.539,21.357,38.465,38.432,0,68.3,0,68.3Z" transform="" fill={chapterInfo.color_primary}/>
-                  <path id="color2" data-name="color2" d="M68.3,0V68.3H0Z" fill={chapterInfo.color_secondary}/>
+            {(chapterInfo.color_primary && chapterInfo.color_secondary) &&
+              <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="68" height="68" viewBox="0 0 68 68">
+                <defs>
+                  <clipPath id="clipPath">
+                    <rect id="rectangleMask" data-name="rectangleMask" width="68" height="68" rx="7"/>
+                  </clipPath>
+                </defs>
+                <g id="chapterColors" data-name="chapterColors">
+                  <g id="mask1" data-name="mask1" clipPath="url(#clipPath)">
+                    <path id="color1" data-name="color1" d="M0,0H68.3S55.539,21.357,38.465,38.432,0,68.3,0,68.3Z" transform="" fill={chapterInfo.color_primary}/>
+                    <path id="color2" data-name="color2" d="M68.3,0V68.3H0Z" fill={chapterInfo.color_secondary}/>
+                  </g>
                 </g>
-              </g>
-            </svg>
+              </svg>
+            }
           </div>
           <div>
-            <h1 className={`chapter-title ${chapterTitleLong}`}>{chapterInfo.school}</h1>
+            <h1 className={String(chapterInfo.school).length > 46 ? ('chapter-title chapter-title-long') : ('chapter-title')}>{chapterInfo.school}</h1>
             <div className="chapter-info">
-              <h2 className="chapter-location">{chapterLocation}</h2>
-              <h2 className="chapter-region">{chapterRegion}</h2>
+              {(() => {
+                let chapterLocation;
+                if(chapterInfo.country) {
+                  chapterLocation = chapterInfo.country;
+                  if(chapterInfo.state) {
+                    chapterLocation = `${chapterInfo.state} - ` + chapterLocation;
+                    if(chapterInfo.city) {
+                      chapterLocation = `${chapterInfo.city}, ` + chapterLocation;
+                    }
+                  }
+                }
+                return <h2 className="chapter-location">{chapterLocation}</h2>;
+              })()}
+              {chapterInfo.region &&
+                <h2 className="chapter-region">{chapterInfo.region} Conference</h2>
+              }
             </div>
           </div>
         </div>
